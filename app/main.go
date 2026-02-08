0885e8eec15b4a9d7d28528530d10d32b84a105a
Sam3992 <samjoyemail@gmail.com>
2026-02-08 17:17:07 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -46,69 +46,86 @@ func main() {
 	if apiKey == "" {
 		panic("Env variable OPENROUTER_API_KEY not found")
 	}
-
-	client := openai.NewClient(option.WithAPIKey(apiKey), option.WithBaseURL(baseUrl))
-	resp, err := client.Chat.Completions.New(context.Background(),
-		openai.ChatCompletionNewParams{
-			// Model: "openrouter/free",
-			Model: "anthropic/claude-haiku-4.5",
-			Messages: []openai.ChatCompletionMessageParamUnion{
-				{
-					OfUser: &openai.ChatCompletionUserMessageParam{
-						Content: openai.ChatCompletionUserMessageParamContentUnion{
-							OfString: openai.String(prompt),
-						},
-					},
+	conversation := []openai.ChatCompletionMessageParamUnion{
+		{
+			OfUser: &openai.ChatCompletionUserMessageParam{
+				Content: openai.ChatCompletionUserMessageParamContentUnion{
+					OfString: openai.String(prompt),
 				},
 			},
-			Tools: []openai.ChatCompletionToolUnionParam{
-				openai.ChatCompletionFunctionTool(openai.FunctionDefinitionParam{
-					Name:        "Read",
-					Description: openai.String("Read and return the contents of a file"),
-					Parameters: openai.FunctionParameters{
-						"type": "object",
-						"properties": map[string]any{
-							"file_path": map[string]any{
-								"type":        "string",
-								"description": "The path to the file to read",
-							},
-						},
-						"required": []string{"file_path"},
-					},
-				}),
-			},
 		},
-	)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
 	}
-	if len(resp.Choices) == 0 {
-		panic("No choices in response")
-	}
-
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	//fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
-	msg := resp.Choices[0].Message
-	if msg.JSON.ToolCalls.Valid() {
-		for _, tc := range msg.ToolCalls {
-			if tc.Type == "function" {
-				if tc.Function.Name == "Read" {
-					var args readArgs
-					err := json.Unmarshal([]byte(tc.Function.Arguments), &args)
-					if err != nil {
-						log.Fatal(err)
-					}
-					content, err := readFileTool(args.FilePath)
-					fmt.Println(content)
-				}
-
-			}
+	client := openai.NewClient(option.WithAPIKey(apiKey), option.WithBaseURL(baseUrl))
+	for {
+		resp, err := client.Chat.Completions.New(context.Background(),
+			openai.ChatCompletionNewParams{
+				Model: "openrouter/free", // using this for local testing
+				// Model: "anthropic/claude-haiku-4.5",'
+				Messages: conversation,
+				Tools: []openai.ChatCompletionToolUnionParam{
+					openai.ChatCompletionFunctionTool(openai.FunctionDefinitionParam{
+						Name:        "Read",
+						Description: openai.String("Read and return the contents of a file"),
+						Parameters: openai.FunctionParameters{
+							"type": "object",
+							"properties": map[string]any{
+								"file_path": map[string]any{
+									"type":        "string",
+									"description": "The path to the file to read",
+								},
+							},
+							"required": []string{"file_path"},
+						},
+					}),
+				},
+			},
+		)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			os.Exit(1)
 		}
-	} else {
-		fmt.Print(resp.Choices[0].Message.Content)
-	}
+		if len(resp.Choices) == 0 {
+			panic("No choices in response")
+		}
 
+		msg := resp.Choices[0].Message
+		conversation = append(conversation, openai.ChatCompletionMessageParamUnion{
+			OfAssistant: &openai.ChatCompletionAssistantMessageParam{
+				Content: openai.ChatCompletionAssistantMessageParamContentUnion{
+					OfString: openai.String(msg.Content),
+				},
+			},
+		})
+
+		if msg.JSON.ToolCalls.Valid() {
+			for _, tc := range msg.ToolCalls {
+				if tc.Type == "function" {
+					if tc.Function.Name == "Read" {
+						var args readArgs
+						err := json.Unmarshal([]byte(tc.Function.Arguments), &args)
+						if err != nil {
+							log.Fatal(err)
+						}
+						fileContent, err := readFileTool(args.FilePath)
+						// fmt.Println(fileContent)
+						conversation = append(conversation, openai.ChatCompletionMessageParamUnion{
+							OfTool: &openai.ChatCompletionToolMessageParam{
+								Content: openai.ChatCompletionToolMessageParamContentUnion{
+									OfString: openai.String(fileContent),
+								},
+								ToolCallID: tc.ID,
+							},
+						})
+
+					}
+
+				}
+			}
+		} else {
+			fmt.Print(resp.Choices[0].Message.Content)
+			break
+		}
+	}
 }
 
 func readFileTool(filePath string) (string, error) {
