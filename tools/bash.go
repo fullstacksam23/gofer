8b0995c6cb9b355183fea0c450ff2beca5622ec7
Sam3992 <samjoyemail@gmail.com>
2026-02-11 19:35:34 +0530

    fix: add cross-platform support to BashTool (closes #1)

--- tools/bash.go
+++ tools/bash.go
@@ -2,18 +2,24 @@ package tools
 
 import (
 	"bytes"
+	"os"
 	"os/exec"
 )
 
 func BashTool(command string) (string, error) {
-	cmd := exec.Command("sh", "-c", command)
+	var cmd *exec.Cmd
+
+	if os.Getenv("OS") == "Windows_NT" {
+		cmd = exec.Command("cmd", "/C", command)
+	} else {
+		cmd = exec.Command("sh", "-c", command)
+	}
 
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
 	err := cmd.Run()
-
 	output := stdout.String() + stderr.String()
 
 	if err != nil {
